1d94db9c2ef1ab71c10a5dacf3f73c65a6105944
Filip Jirsák <jirsakf@oksystem.cz>
2022-05-04 10:42:28 +0200

    Připravený prázdný projekt.

---
+++ src/main/java/cz/czechitas/lekce5/HlavniProgram.java
@@ -0,0 +1,17 @@
+package cz.czechitas.lekce5;
+
+/**
+ * Hlaví třída pro lekci 5.
+ */
+public class HlavniProgram {
+
+    /**
+     * Spouštěcí metoda celé aplikace.
+     *
+     * @param args
+     */
+    public static void main(String... args) {
+        //TODO tady bude náš kód
+    }
+
+}
